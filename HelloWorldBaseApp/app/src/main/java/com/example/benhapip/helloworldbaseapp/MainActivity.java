7d8a358703bea0527bec26686c2ec429c65e9f5f
ben-hapip <benhapip@gmail.com>
2017-02-15 22:25:19 -0600

    added bluetooth functionality

--- HelloWorldBaseApp/app/src/main/java/com/example/benhapip/helloworldbaseapp/MainActivity.java
+++ HelloWorldBaseApp/app/src/main/java/com/example/benhapip/helloworldbaseapp/MainActivity.java
@@ -1,30 +1,97 @@
 package com.example.benhapip.helloworldbaseapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Tagdoe";
     public Button but1;
+    BluetoothAdapter btw;
     public void init(){
         but1 = (Button) findViewById(R.id.but1);
+        btw = BluetoothAdapter.getDefaultAdapter();
+
         but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent second = new Intent(MainActivity.this,SecondActivity.class);
                 second.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(second);
+                enabledDisabled();
             }
         });
     }
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver1 = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(btw.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE
+                ,btw.ERROR);
+
+                switch(state){
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.d(TAG, "onReceive: STATE OFF");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        Log.d(TAG, "mReceiver1: STATE TURNING OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.d(TAG, "mReceiver1: STATE ON");
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        Log.d(TAG, "mReceiver1: STATE Turning ON");
+                        break;
+        }
+
+    }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+    }
+    @Override
+    protected void onDestroy(){
+        Log.d(TAG,"onDestroy called.");
+        super.onDestroy();
+        unregisterReceiver(mReceiver1);
+    }
+    public void enabledDisabled(){
+        if(btw == null) {
+            Log.d(TAG, "enableDisabled: Does not have BT capabilities.");
+        }
+        else if(!btw.isEnabled()){
+            Log.d(TAG,"enableDisabledBT: Enabling BT");
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBTIntent);
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mReceiver1, BTIntent);
+        }
+        else if(btw.isEnabled()){
+            btw.disable();
+
+            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mReceiver1, BTIntent);
+        }
+
+
 
     }
 }
--- HelloWorldBaseApp/app/src/main/java/com/example/benhapip/helloworldbaseapp/SecondActivity.java
+++ HelloWorldBaseApp/app/src/main/java/com/example/benhapip/helloworldbaseapp/SecondActivity.java
@@ -8,22 +8,24 @@ import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
-    Button but2;
-    public void init() {
-        but2 = (Button) findViewById(R.id.but2);
-        but2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent first = new Intent(SecondActivity.this, MainActivity.class);
-                first.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(first);
-            }
-        });
-    }
+//    Button but2;
+//    public void init() {
+//        but2 = (Button) findViewById(R.id.but2);
+//        but2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent first = new Intent(SecondActivity.this, MainActivity.class);
+//                first.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                startActivity(first);
+//            }
+//        });
+
+  //  }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        init();
+      //  init();
+
     }
 }
